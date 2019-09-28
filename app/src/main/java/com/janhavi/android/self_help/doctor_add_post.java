c05550423899d381a9fb75712e954c5d651cc0cd
apoorvakhuspe <apoorvakhuspe@gmail.com>
2019-09-29 01:44:25 +0530

    Initial commit

---
+++ app/src/main/java/com/janhavi/android/self_help/doctor_add_post.java
@@ -0,0 +1,14 @@
+package com.janhavi.android.self_help;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class doctor_add_post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_add_post);
+    }
+}
+
